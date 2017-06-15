8e85552eda4b5bb71f53ea6faacf8fd3f8110be1
aisa.tashmatova <aisa.tashmatova@gmail.com>
2017-06-15 17:13:55 +0600

    I committed Grid View

---
+++ GridView/app/src/main/java/aisa/gridview/CustomGrid.java
@@ -0,0 +1,65 @@
+package aisa.gridview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by admin on 6/15/17.
+ */
+
+public class CustomGrid extends BaseAdapter {
+    private Context mContext;
+    private final String[] web;
+    private final int[] Imageid;
+
+    public CustomGrid(Context c,String[] web,int[] Imageid ) {
+        mContext = c;
+        this.Imageid = Imageid;
+        this.web = web;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return web.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            grid = new View(mContext);
+            grid = inflater.inflate(R.layout.cell, null);
+            TextView textView = (TextView) grid.findViewById(R.id.textView);
+            ImageView imageView = (ImageView)grid.findViewById(R.id.imageView);
+            textView.setText(web[position]);
+            imageView.setImageResource(Imageid[position]);
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+}
---
+++ GridView/app/src/main/java/aisa/gridview/MainActivity.java
@@ -0,0 +1,58 @@
+package aisa.gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] web = {
+            "Typing",
+            "Consultation",
+            "Registration",
+            "Ranking",
+            "Security",
+            "Achievements",
+            "Departments",
+            "Finance",
+            "Bank"
+
+
+    } ;
+    int[] imageId = {
+            R.drawable.img,
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+            R.drawable.img7,
+            R.drawable.img8
+
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridView = (GridView)findViewById(R.id.gridView);
+
+        CustomGrid adapter = new CustomGrid(MainActivity.this, web, imageId);
+        gridView=(GridView)findViewById(R.id.gridView);
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
